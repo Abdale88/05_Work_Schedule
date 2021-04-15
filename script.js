b9c2944143e90975145882e7913893d96918a018
Abdale Hassan <cabdale989@gmail.com>
2021-04-14 23:35:39 -0500

    Create variables in javaScript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+var container = document.querySelector("#container");
+var description = document.querySelector("#description");
+var saveBtn = document.querySelector("#saveBtn");
+
+
+
 $('#currentDay').text(dayjs().format('MMM Do, YYYY'));
