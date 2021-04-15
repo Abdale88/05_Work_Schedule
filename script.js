c2e7ae1a55b7fd2749d36805092931bec9b843de
Abdale Hassan <cabdale989@gmail.com>
2021-04-15 14:45:19 -0500

    add an event listener to save button and a create a function for the local storage

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 var container = document.querySelector("#container");
 var description = document.querySelector("#description");
-var saveBtn = document.querySelector("#saveBtn");
+var saveBtn = document.getElementById("saveBtn");
 
+
+
+saveBtn.addEventListener("click", function(event){
+   event.preventDefault();
+   console.log(localStorage);
+   var descriptionObject = {
+       eventDes: description,
+   }
+
+   localStorage.setItem("description", JSON.stringify(descriptionObject));
+   renderDes();
+});
+
+
+function renderDes(){
+    var x = JSON.parse(localStorage.getItem("description"));
+    if(x !== null){
+        document.querySelector(".description").textContent = "";
+    }
+}
+
+
+
+// var time = dayjs().format('h A');
+// $('.hour').text(time);
 
 
 $('#currentDay').text(dayjs().format('MMM Do, YYYY'));
