1be451c09150c0207084123cb5838df8722bd398
Abdale Hassan <cabdale989@gmail.com>
2021-04-16 16:05:09 -0500

    testing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var container = document.querySelector("#container");
 var description = document.querySelector("#description");
-// var saveBtn = document.querySelector("saveBtn");
+
 
 var storage = {};
 
@@ -11,29 +11,18 @@ document.querySelectorAll('.saveBtn').forEach(item => {
         var parent = button.closest("div.row");
 
         var id = parent.id;
+        console.log("this >> " + id.value);
         var textContent = parent.querySelector("textarea").value;
-
-        storage[id] =  textContent;
+        storage[id] =  textContent; 
 
         localStorage.setItem("storage", JSON.stringify(storage)); 
+
     });
 })
 
-document.querySelectorAll('.saveBtn').forEach(item => {
-    item.addEventListener('click', event => {
-        var button = event.target;
-        var parent = button.closest("div.row");
 
-        var id = parent.id;
-        var textContent = parent.querySelector("textarea").value;
-
-        storage[id] =  textContent;
 
         var getEl = JSON.parse(localStorage.getItem("storage")); 
-    });
-})
-
-
 
 
 // var time = dayjs().format('h A');
